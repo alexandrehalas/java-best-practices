d1d8f5ee684fd47ae949af6ca94ddeea72a6a86d
Alexandre Halas <alexandrehalas@gmail.com>
2025-01-15 23:15:23 -0300

    feat: Using switch expressions

--- src/main/java/br/com/halas/AdopetConsoleApplication.java
+++ src/main/java/br/com/halas/AdopetConsoleApplication.java
@@ -10,38 +10,37 @@ public class AdopetConsoleApplication {
 
         System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
         try {
-            int opcaoEscolhida = 0;
-            while (opcaoEscolhida != 5) {
-                System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
-                System.out.println("1 -> Listar abrigos cadastrados");
-                System.out.println("2 -> Cadastrar novo abrigo");
-                System.out.println("3 -> Listar pets do abrigo");
-                System.out.println("4 -> Importar pets do abrigo");
-                System.out.println("5 -> Sair");
+            int optionChosen = 0;
+            while (optionChosen != 5) {
 
-                String textoDigitado = new Scanner(System.in).nextLine();
-                opcaoEscolhida = Integer.parseInt(textoDigitado);
+                showMenu();
 
-                if (opcaoEscolhida == 1) {
-                    commandExecutor.executeCommand(new ListShelterCommand());
-                } else if (opcaoEscolhida == 2) {
-                    commandExecutor.executeCommand(new RegisterShelterCommand());
-                } else if (opcaoEscolhida == 3) {
-                    commandExecutor.executeCommand(new ListPetCommand());
-                } else if (opcaoEscolhida == 4) {
-                    commandExecutor.executeCommand(new ImportPetsCommand());
-                } else if (opcaoEscolhida == 5) {
-                    break;
-                } else {
-                    System.out.println("NÚMERO INVÁLIDO!");
-                    opcaoEscolhida = 0;
+                String text = new Scanner(System.in).nextLine();
+                optionChosen = Integer.parseInt(text);
+
+                switch (optionChosen) {
+                    case 1 -> commandExecutor.executeCommand(new ListShelterCommand());
+                    case 2 -> commandExecutor.executeCommand(new RegisterShelterCommand());
+                    case 3 -> commandExecutor.executeCommand(new ListPetCommand());
+                    case 4 -> commandExecutor.executeCommand(new ImportPetsCommand());
+                    case 5 -> System.exit(0);
+                    default -> optionChosen = 0;
                 }
             }
+
             System.out.println("Finalizando o programa...");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private static void showMenu() {
+        System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
+        System.out.println("1 -> Listar abrigos cadastrados");
+        System.out.println("2 -> Cadastrar novo abrigo");
+        System.out.println("3 -> Listar pets do abrigo");
+        System.out.println("4 -> Importar pets do abrigo");
+        System.out.println("5 -> Sair");
+    }
 
 }
